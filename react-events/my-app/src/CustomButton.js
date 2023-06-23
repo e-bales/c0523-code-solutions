e719249be572c54e99c439ff54bcff733a3050fd
Eli Bales <elibales21@gmail.com>
2023-06-23 01:08:27 +0000

    completed react-events exercises

---
+++ react-events/my-app/src/CustomButton.js
@@ -0,0 +1,11 @@
+export default function CustomButton({ text, color, onCustomClick }) {
+  return (
+    <button
+      onClick={() => {
+        onCustomClick(text);
+      }}
+      style={{ backgroundColor: color }}>
+      {text}
+    </button>
+  );
+}
