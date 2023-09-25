4c7afd20f4e35b707f4ac00e216e9a7e21aa7309
Eli Bales <elibales21@gmail.com>
2023-09-25 19:43:21 +0000

    added media queries

--- tailwind/my-app/src/App.js
+++ tailwind/my-app/src/App.js
@@ -6,30 +6,33 @@ import './App.css';
 function App() {
   return (
     <div className="App h-screen">
-      <header className="header-wrap py-4">
+      <header className="header-wrap">
         <div className="header">
           <h3 className="header-link">About</h3>
-          <h3 className="header-link">Work</h3>
-          <h3 className="header-link">Github</h3>
+          <h3 className="header-link border-lr">Work</h3>
+          <h3 className="header-link border-r">Github</h3>
           <h3 className="header-link">Resume</h3>
         </div>
       </header>
       <section className="body">
         <div className="body-wrap">
           <div className="row">
-            <h1 className="title">Hi, I'm Eli!</h1>
+            <h1 className="title">Hi, I'm Eli,</h1>
           </div>
           <div className="row">
             <h2 className="sub-title">Welcome to my page!</h2>
           </div>
           <div className="row">
             <h3 className="info">
-              Thanks for visiting! I'm a Software Engineer with a background in
-              AI and Web Development. Check out some of my work in the links
-              above, or download my Resume to learn more about me. Want to get
-              in contact? Connect with me on LinkedIn, or send me an email at
+              I'm a Software Engineer with a background in AI and Web
+              Development. Check out some of my work in the links above, or
+              download my Resume to learn more about me. Want to get in contact?
+              Connect with me on LinkedIn, or send me an email at
               elibales21@gmail.com.
             </h3>
+          </div>
+          <div className="row">
+            <div className="bar"></div>
           </div>
         </div>
       </section>
